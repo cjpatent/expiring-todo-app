5791972304fa86def128116817a11d0b760c776c
cjpatent <christopher@patenthelp.co.uk>
2025-05-13 14:55:29 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-import { useState, useEffect } from "react";
-import { Card, CardContent } from "../components/ui/card";
-import { Input } from "../components/ui/input";
-import { Button } from "../components/ui/button";
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "../components/ui/select";
-import { Calendar } from "../components/ui/calendar";
-
-/* Remaining code unchanged for brevity... (same as in canvas) */
